4030fa31ffaa238094fb5528121388f603445b51
beloborod <strig-style@yandex.ru>
2019-04-09 17:28:00 +0300

    add comments)

--- DEcoding/hemming/main.py
+++ DEcoding/hemming/main.py
@@ -2,35 +2,38 @@ import random
 from DEcoding.hemming.math_funcs import *
 
 if __name__ == '__main__':
+
     message = "\nType string for coding (for example your initials), but only one type of symbols " \
               "(for example numbers, letters of one alphabet): "
-    input_string = str(input(message))
+
+    input_string = str(input(message))  # get message-string
+
     symbols_codes_list = []
 
     add = None
-    while add != "" and add != "Y" and add != "N":
+    while add != "" and add != "Y" and add != "N":  # add random error in message or no
         add = str(input("\nAdd a random error in message? ('y'/'n' or 'enter' for 'n'): "))
         if add == "":
             add = "N"
         add = add.upper()
         print(add)
 
-    for symbol in input_string:
+    for symbol in input_string:  # get symbol's code, transformation into binary and delete "0b" prefix
         symbols_codes_list.append(bin(ord(symbol))[2:])
 
     symbols_length = len(symbols_codes_list[0])
-    bin_str = ""
 
+    bin_str = ""
     for code in symbols_codes_list:
         bin_str += code
 
     print(f"\nMessage in binary code: {int(bin_str)}\n")
 
-    bin_list = list(bin_str)
+    bin_list = list(bin_str)    # transform binary message into list and set type of values in list as "int"
     bin_list = list(map(int, bin_list))
-    ret, powers = hamming_coding(bin_list)
+    ret = hamming_coding(bin_list)  # get coded message
 
-    if add == "Y":
+    if add == "Y":  # add random error
         random_error = random.randint(0, len(ret)-1)
 
         print(f"Error in bite with number: {random_error+1}\n")
@@ -44,7 +47,7 @@ if __name__ == '__main__':
         print_list(ret)
         print("\n")
 
-    decoded = hamming_decoding(ret, powers)
+    decoded = hamming_decoding(ret)  # get decoded message with fixed error, if it's exist
     decoded_str = ""
     bite_str = ""
 
